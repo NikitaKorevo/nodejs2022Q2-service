bfc238158390c094224e33122c4d474e86ada714
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2022-07-17 19:45:59 +0300

    feat: add track route

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArtistsModule } from './module/artists/artists.module';
 import { AlbumsModule } from './module/albums/albums.module';
+import { TracksModule } from './module/tracks/tracks.module';
 
 @Module({
-  imports: [ArtistsModule, AlbumsModule],
+  imports: [ArtistsModule, AlbumsModule, TracksModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/data/database.ts
+++ src/data/database.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Album } from 'src/module/albums/schemas/album.schema';
 import { Artist } from 'src/module/artists/schemas/artist.schema';
+import { Track } from 'src/module/tracks/schemas/track.schema';
 
 @Injectable()
 export class Database {
@@ -16,6 +17,7 @@ export class Database {
       grammy: true,
     },
   ];
+
   albums: Array<Album> = [
     {
       id: 'a61452d7-59f5-4967-9446-d88ba53bf5d4',
@@ -28,6 +30,23 @@ export class Database {
       name: 'Wonderful album',
       year: 2005,
       artistId: 'a8536a2e-5566-4ab9-8414-e84fec7a0f6e',
+    },
+  ];
+
+  tracks: Array<Track> = [
+    {
+      id: 'd546c4ae-a1ec-4ced-931b-c6b13a83be51',
+      name: 'Hello andrey',
+      artistId: 'a8536a2e-5566-4ab9-8414-e84fec7a0f6e',
+      albumId: null,
+      duration: 111,
+    },
+    {
+      id: '42680a7b-050f-46c7-8160-074606d9e166',
+      name: 'Run',
+      artistId: null,
+      albumId: 'a61452d7-59f5-4967-9446-d88ba53bf5d4',
+      duration: 160,
     },
   ];
 }
